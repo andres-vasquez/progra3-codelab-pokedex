a4af921bf6c3f489034416ea02eef039c1b9afa8
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2019-04-19 11:56:37 -0400

    Main Activity start

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/MainActivity.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/MainActivity.java
@@ -1,13 +1,41 @@
 package bo.upb.programacion3.codelabpokedex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ActionBarDrawerToggle drawerToggle;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.toolbar);
+        this.drawerLayout = findViewById(R.id.navigationDrawer);
+
+        if (this.toolbar != null) {
+            this.toolbar.setTitle(R.string.main_activity_title);
+            this.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorWhite));
+
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+            drawerToggle.setDrawerIndicatorEnabled(false);
+            drawerToggle.setHomeAsUpIndicator(R.drawable.icon_hamburger);
+            drawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+        }
     }
 }
